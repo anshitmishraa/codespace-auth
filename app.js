07c2a7818d64e9ce3bd9fab520f7f77c4f74d2ff
Anshit Mishra <54078251+anshitmishraa@users.noreply.github.com>
2022-11-22 02:58:48 +0000

    Register Page

--- app.js
+++ app.js
@@ -2,9 +2,23 @@ require('dotenv').config()
 const express = require('express')
 
 const app = express()
+app.use(express.json())
 
 app.get("/", ((req,res) => {
     res.send("<h1>Server is Working</h1>")
 }))
+
+app.post("/register", async (req,res) => {
+    try {
+        // get all data from body
+        // all the data should exists
+        // check if user already exists
+        // encrypt the password
+        // save the user in DB
+        // generate a token for user and send it
+    } catch (error) {
+        console.log(error);
+    }
+})
 
 module.exports = app
